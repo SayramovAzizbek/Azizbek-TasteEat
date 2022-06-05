0e2be28b35be3605b70567252f9fded5f548a0b0
SayramovAzizbek <sayramovazizbek@gmail.com>
2022-06-05 19:28:30 +0500

    Optimize the function and change Html structure

--- js/main.js
+++ js/main.js
@@ -16,14 +16,35 @@ var swiper = new Swiper(".mySwiper", {
 
 const InputFormBtn = document.querySelector('.input-form-btn')
 const FooterBtn = document.querySelector('.footer-input-btn')
-// const footerInputForm = document.querySelector('.footer-input-form')
+const footerInputForm = document.querySelector('.footer-input-form')
+const nameInput = document.querySelector('.NameInput')
+const emailInput = document.querySelector('.EmailInput')
+const numberInput = document.querySelector('.NumberInput')
+const phoneNumberInput = document.querySelector('.PhoneNumberInput')
+const reserveTimeInput = document.querySelector('.ReserveTimeInput')
+const FormInput = document.querySelector('.formInput')
 
 InputFormBtn.addEventListener('click', () => {
-  InputFormBtn.innerHTML= "You Booked"
+    const NameInput = nameInput.value
+    const EmailInput = emailInput.value
+    const ReserveTimeInput = reserveTimeInput.value
+    const PhoneNumberInput = phoneNumberInput.value
+    const NumberInput = numberInput.value
+  if( NameInput && EmailInput && PhoneNumberInput && ReserveTimeInput && NumberInput != '') {
+    InputFormBtn.innerHTML= "You Booked"
     alert("You Booked a Table")
+  }else{
+    alert("If you want to reserve fill out the form till the end")
+  }
 })
 
 FooterBtn.addEventListener('click', () => {
-  FooterBtn.innerHTML= "Subcribed"
-  alert("You subscribed \n\Thank you for supporting")
+  const InputForm = footerInputForm.value
+  if(InputForm != '') {
+    FooterBtn.innerHTML= "Subcribed"
+    alert("You subscribed")
+  }
+  else{
+    alert("Enter Your Email For Subscribe")
+  }
 })
